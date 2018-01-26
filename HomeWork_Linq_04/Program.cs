7b01f4d503006308bc7054050180f286ad6898b5
СидоренкоЕ <@403_4_66>
2018-01-26 12:30:03 +0600

    Успешная выгрузка дынных в XML

--- HomeWork_Linq_04/Program.cs
+++ HomeWork_Linq_04/Program.cs
@@ -15,7 +15,13 @@ namespace HomeWork_Linq_04
         {
             try
             {
-                Task_D();
+                //Task_A();
+                Task_B();
+                //Task_C();
+                //Task_D();
+                //Task_E();
+                //Task_F();
+                //Task_G();
             }
             catch (Exception ex)
             {
@@ -31,7 +37,7 @@ namespace HomeWork_Linq_04
                 XDocument xdoc = new XDocument(
                     new XElement("Area",
                  new XElement("PavillionId", item.PavilionId), new XElement("AreaId", item.AreaId), new XElement("AreaName", item.Name)));
-                xdoc.Save(item.PavilionId.ToString());
+                xdoc.Save("Task_A_File"+item.PavilionId.ToString());
 
             }
         }
@@ -39,7 +45,7 @@ namespace HomeWork_Linq_04
         private static void Task_B()
         {
             //Directory.CreateDirectory("Area");
-            DirectoryInfo dir = new DirectoryInfo("Task_C_Directory");
+            DirectoryInfo dir = new DirectoryInfo("Task_B_Directory");
             dir.Create();
             List<Area> a = new List<Area>();
             try
@@ -57,7 +63,18 @@ namespace HomeWork_Linq_04
                 foreach (Area item in a)
 
                 {
-                    Directory.CreateDirectory(dir.FullName + @"\" + item.Name);
+                    
+                    DirectoryInfo sub_dir = new DirectoryInfo(dir.FullName + @"\" + "Areas");
+                    sub_dir.Create();
+                    DirectoryInfo sub_subDir = new DirectoryInfo(sub_dir.FullName + @"\" + item.Name);
+                    sub_subDir.Create();
+                    //Directory.CreateDirectory(sub_dir.FullName + @"\" + item.Name);
+                    XDocument xdoc = new XDocument(new XElement("Area",
+                        new XElement("AreaId", item.AreaId),
+                        new XElement("AreaName", item.FullName),
+                        new XElement("AreaIp", item.IP)));
+
+                    xdoc.Save(sub_subDir.FullName + @"\" + "_" + item.AreaId);
                 }
                 Console.WriteLine("Директории были успешно добавлены!");
             }
@@ -129,13 +146,103 @@ namespace HomeWork_Linq_04
                 sub_dir.Create();
                 DirectoryInfo sub_subDir = new DirectoryInfo(sub_dir.FullName + @"\" + item.Name);
                 sub_subDir.Create();
-                XDocument xdoc = new XDocument(new XElement("Area",
+                XDocument xdoc = new XDocument(new XElement("AreaAndTimer",
            new XElement("AreaName", item.Name),
            new XElement("TimerUserId", item.UserId),
            new XElement("TimerDateStart", item.DateStart)));
                 xdoc.Save(sub_subDir.FullName + @"\" + "_" + item.UserId.ToString());
             }
             Console.WriteLine("Данные успешно выгружены!\nНо База Чето Не оч)");
+        }
+        private static void Task_E()
+        {
+            Console.WriteLine("Выгружаем данные...");
+            DirectoryInfo dir = new DirectoryInfo("Task_E_Directory");
+            dir.Create();
+            var a = db.Timers.Where(w => w.DateFinish == null).Select(s => new
+            {
+                s.TimerId,
+                s.DateStart,
+                s.AreaId
+            });
+            foreach (var item in a.ToList())
+            {
+                DirectoryInfo sub_dir = new DirectoryInfo(dir.FullName + @"\" + "Timers");
+                sub_dir.Create();
+                DirectoryInfo sub_subDir = new DirectoryInfo(sub_dir.FullName + @"\" + item.AreaId);
+                sub_subDir.Create();
+                XDocument xdoc = new XDocument(new XElement("Timer",
+           new XElement("AreaId", item.AreaId),
+           new XElement("TimerId", item.TimerId),
+           new XElement("DateStart", item.DateStart)));
+                xdoc.Save(sub_subDir.FullName + @"\" + "_" + item.
+                    AreaId.ToString()+".xml");
+            }
+            Console.WriteLine("Данные были выгружены успешно");
+
+        }
+        private static void Task_F()
+        {
+            Console.WriteLine("Выгружаем данные...");
+            DirectoryInfo dir = new DirectoryInfo("Task_F_Directory");
+            dir.Create();
+            var a = db.Timers.Where(w => w.DateFinish != null).Select(s => new
+            {
+                s.TimerId,
+                s.DateStart,
+                s.AreaId,
+                s.DateFinish
+            });
+            Console.WriteLine("Вот сколько нужно выгрузить строк : "+a.Count());
+            foreach (var item in a.ToList())
+            {
+                DirectoryInfo sub_dir = new DirectoryInfo(dir.FullName + @"\" + "Timers");
+                sub_dir.Create();
+                DirectoryInfo sub_subDir = new DirectoryInfo(sub_dir.FullName + @"\" + item.AreaId);
+                sub_subDir.Create();
+                XDocument xdoc = new XDocument(new XElement("Timer",
+           new XElement("AreaId", item.AreaId),
+           new XElement("TimerId", item.TimerId),
+           new XElement("DateStart", item.DateStart)));
+                xdoc.Save(sub_subDir.FullName + @"\" + "_" + item.
+                    AreaId.ToString() + ".xml");
+            }
+            Console.WriteLine("Данные были выгружены успешно");
+
+        }
+        private static void Task_G()
+        {
+            Console.WriteLine("Выгружаем данные...");
+            DirectoryInfo dir = new DirectoryInfo("Task_G_Directory");
+            dir.Create();
+            var a = db.Areas.Select(s => new
+            {
+                s.AreaId,
+                s.IP,
+                s.Name
+            });
+            Console.WriteLine("Вот сколько нужно выгрузить строк : " + a.Count());
+            XNamespace ns = "http://logbook.itstep.org/";
+            XNamespace areaNs = "area";
+            foreach (var item in a.ToList())
+            {
+                DirectoryInfo sub_dir = new DirectoryInfo(dir.FullName + @"\" + "Timers");
+                sub_dir.Create();
+                DirectoryInfo sub_subDir = new DirectoryInfo(sub_dir.FullName + @"\" + item.Name);
+                sub_subDir.Create();
+                XDocument xdoc = new XDocument(new XElement(ns+  "Area",
+           new XElement(areaNs + "AreaId", item.AreaId),
+           new XElement(areaNs+"IP", item.IP),
+           new XElement(areaNs+"Name", item.Name)));
+                xdoc.Save(sub_subDir.FullName + @"\" + "_" + item.
+                    AreaId.ToString() + ".xml");
+            }
+            Console.WriteLine("Данные были выгружены успешно");
+
+        }
+        private static void Task_4_A()
+        {
+
         }
     }
 }
